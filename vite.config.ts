f9feaaeb59a74a0f7cccbd6ef1aa84f5b7cf8e60
mlafefon <51256464+mlafefon@users.noreply.github.com>
2025-11-06 19:59:25 +0200

    1.0.36 סרגל בצבע

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import { URL, fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 
 
@@ -16,7 +16,8 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          // Fix: `__dirname` is not available in ES modules. `import.meta.url` is the modern replacement.
+          '@': fileURLToPath(new URL('.', import.meta.url)),
         }
       }
     };
