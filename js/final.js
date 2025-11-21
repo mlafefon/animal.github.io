80e6a0b83e72bab09d673a1d60f0d1d1b242bb57
mlafefon <51256464+mlafefon@users.noreply.github.com>
2025-11-21 19:26:06 +0200

    1.1.73" ביטול הימור

--- js/final.js
+++ js/final.js
@@ -2,6 +2,7 @@
 
 
 
+
 import { getTeamsWithScores, getFinalQuestionData, adjustScoreForTeam, clearGameState, broadcastGameState } from './game.js';
 import { playSound } from './audio.js';
 import { showLinkModal } from './ui.js';
@@ -77,7 +78,7 @@ function renderBettingCards() {
                         <div class="bet-control locked">
                             <div class="locked-status">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#4CAF50"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
-                                <span class="locked-amount">${currentBet}</span>
+                                <span class="locked-amount">ההימור התקבל</span>
                             </div>
                             <button class="unlock-bet-btn" data-index="${team.index}" title="בטל הימור ושחרר">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
